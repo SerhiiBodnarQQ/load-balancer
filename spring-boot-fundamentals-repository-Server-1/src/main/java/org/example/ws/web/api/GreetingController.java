a911596748edddd567e96eaca71012646aadf66e
SerhiiBodnarQQ <128730194+SerhiiBodnarQQ@users.noreply.github.com>
2023-03-23 20:07:18 +0200

    Add files via upload

---
+++ spring-boot-fundamentals-repository-Server-1/src/main/java/org/example/ws/web/api/GreetingController.java
@@ -0,0 +1,60 @@
+package org.example.ws.web.api;
+
+import org.example.ws.model.Greeting;
+import org.example.ws.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+public class GreetingController {
+
+    @Autowired
+    private GreetingService greetingService;
+
+    @RequestMapping(
+            value = "/api/greetings",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Collection<Greeting>> getGreetings() {
+        Collection<Greeting> greetings = greetingService.findAll();
+
+        Greeting greeting1 = new Greeting();
+        greeting1.setText("Server №1");
+        greetings.add(greeting1);
+        return new ResponseEntity<Collection<Greeting>>(greetings,
+                HttpStatus.OK);
+    }
+
+    @RequestMapping(
+            value = "/api/greetings/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Greeting> getGreeting(@PathVariable("id") Long id) {
+
+        Greeting greeting = greetingService.findOne(id);
+        if (greeting == null) {
+            return new ResponseEntity<Greeting>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);
+    }
+
+    @RequestMapping(
+            value = "/api/greetings",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Greeting> createGreeting(
+            @RequestBody Greeting greeting) {
+
+        Greeting savedGreeting = greetingService.create(greeting);
+
+        return new ResponseEntity<Greeting>(savedGreeting, HttpStatus.CREATED);
+    }
+
+}
